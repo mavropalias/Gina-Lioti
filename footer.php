972d9b9acc1c82e5fafad4259220cc2ab7830425
Kostas Mavropalias <mavropalias@gmail.com>
2016-11-09 13:36:39 +0000

    updated header

--- footer.php
+++ footer.php
@@ -1,6 +1,29 @@
+    <!-- GET IN TOUCH -->
+    <section>
+        <div class="row">
+            <div class="large-12 columns">
+                <h2>Get in touch</h2>
+            </div>
+        </div>
+        <ul class="list-view">
+            <li class="list-item has-indicator"><a class="contact.html">Send a message</a></li>
+            <li class="list-item"><a href="https://www.facebook.com/ginalioticooking">Facebook</a></li>
+            <li class="list-item"><a href="https://twitter.com/GinaLioti">Twitter</a></li>
+        </ul>
+    </section>
 
-      </div>
-    </div>
+    <!-- COPYRIGHT -->
+    <section>
+        <div class="row">
+            <div class="large-12 columns">
+                <strong>Thank you!</strong> <br>
+                <small>© Gina Lioti. All rights reserved.</small>
+            </div>
+        </div>
+    </section>
+
+    </main>
+  
     <!-- wp_footer START -->
     <?php wp_footer(); ?>
     <!-- wp_footer END -->
@@ -8,11 +31,11 @@
     <!-- Google Analytics FOOTER -->
     <!-- ======================================================================= -->
     <script type="text/javascript">  (function() {
-      var ga = document.createElement('script');     ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:'   == document.location.protocol ? 'https://ssl'   : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-     </script>
+        var ga = document.createElement('script');     ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:'   == document.location.protocol ? 'https://ssl'   : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
 
-  </body>
+    </body>
 </html>
--- header.php
+++ header.php
@@ -1,107 +1,129 @@
-<!DOCTYPE html>
+<!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
 
-  <!-- Title -->
-  <!-- ======================================================================= -->
-  <title><?php wp_title( ' | ', true, 'right' ); ?></title>
+    <!-- Title -->
+    <!-- ======================================================================= -->
+    <title><?php wp_title( ' | ', true, 'right' ); ?></title>
 
-  <!-- Meta -->
-  <!-- ======================================================================= -->
-  <meta charset="<?php bloginfo( 'charset' ); ?>" />
-  <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1, user-scalable=yes">
-  <link rel="alternate" type="application/rss+xml" title="Gina Lioti Cooking" href="<?php echo site_url()."/feed/recipes" ?>" />
-  <meta name="p:domain_verify" content="35207d07bacab562b77eca9b1f27ad02"/>
-  <meta property="fb:pages" content="349519068543688" />
-  <meta name="theme-color" content="#AF1E40">
+    <!-- Meta -->
+    <!-- ======================================================================= -->
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes">
+    <link rel="alternate" type="application/rss+xml" title="Gina Lioti Cooking" href="<?php echo site_url()."/feed/recipes" ?>" />
+    <meta name="p:domain_verify" content="35207d07bacab562b77eca9b1f27ad02"/>
+    <meta property="fb:pages" content="349519068543688" />
 
-  <!-- Icons -->
-  <!-- ======================================================================= -->
-  <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-  <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
-  <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
-  <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
-  <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
-  <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
-  <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
-  <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
-  <link rel="icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+    <!-- THEME COLOR -->
+    <!-- ======================================================================= -->
+    <meta name="theme-color" content="#B3213D"> <!-- Chrome, Firefox OS, Opera and Vivaldi -->
+    <meta name="msapplication-navbutton-color" content="#B3213D"> <!-- Windows Phone -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"> <!-- iOS Safari -->
 
-  <!-- Styling -->
-  <!-- ======================================================================= -->
-  <link href='//fonts.googleapis.com/css?family=RobotoDraft:regular,bold,italic,thin,light,black,medium&amp;lang=en' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+    <!-- APP TAGS -->
+    <!-- ======================================================================= -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
 
-  <!-- WP Head -->
-  <!-- ======================================================================= -->
-  <?php wp_head(); ?>
-  <!-- /wp_head END -->
+    <!-- Icons -->
+    <!-- ======================================================================= -->
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
+    <link rel="icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
 
-  <!-- Google Analytics HEAD -->
-  <!-- ======================================================================= -->
-  <script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-749229-27']);
-    _gaq.push(['_trackPageview']);
-  </script>
+    <!-- Styling -->
+    <!-- ======================================================================= -->
+    <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=PT+Serif" rel="stylesheet">
+
+    <!-- WP Head -->
+    <!-- ======================================================================= -->
+    <?php wp_head(); ?>
+    <!-- /wp_head END -->
+
+    <!-- Google Analytics HEAD -->
+    <!-- ======================================================================= -->
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-749229-27']);
+        _gaq.push(['_trackPageview']);
+    </script>
+
 </head>
 
 <body <?php body_class(); ?>>
-<!-- Sidebar -->
-<!-- ======================================================================= -->
-<header class="header" role="banner">
-  <!-- Branding -->
-  <div id="branding">
-    <div id="site-title">
-      <?php if ( ! is_singular() ) { echo '<h1>'; } ?>
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'ginalioti' ); ?>" rel="home">
-          <img class="app-logo" src="<?php echo get_template_directory_uri(); ?>/images/gina-lioti-logo.svg" alt="<?php echo esc_html( get_bloginfo( 'name' ) ); ?> logo" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?> – <?php bloginfo( 'description' ); ?>">
-        </a>
-      <?php if ( ! is_singular() ) { echo '</h1>'; } ?>
-    </div>
-  </div>
+    <header class="expanded">
+        <div class="row">
+            <div class="columns inner">
+                <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'ginalioti' ); ?>" rel="home">
+                    <img class="app-logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/gina_lioti_chef_logo.svg" alt="Gina Lioti Cooking" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?> – <?php bloginfo( 'description' ); ?>">
+                </a>
 
-  <!-- Navigation -->
-  <nav id="menu" role="navigation">
+                <nav class="show-for-large">
+                    <ul>
+                        <li>
+                            <a href="index.html" class="icon ion-home active" title="Home"></a>
+                            
+                        </li>
+                        <li>
+                            
+                            <a href="search.html" class="icon ion-search" title="Search"></a>
+                        </li>
+                        <li>
+                            
+                            <a href="recipes.html" title="Recipes">Recipes</a>
+                        </li>
+                        <li class="show-for-medium">
+                            
+                            <a href="ingredients.html" title="Recipes">Ingredients</a>
+                        </li>
+                        <li>
+                                <a href="gina-lioti.html" class="show-for-medium">Gina Lioti</a>
+                                <a href="gina-lioti.html" class="show-for-small-only">Gina</a>
+                        </li>
+                    </ul>
+                </nav>
 
-    <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-
-    <!-- Search -->
-    <form action="/" method="get">
-       <div class="row collapse postfix-round search">
-        <div class="small-9 columns">
-          <input type="text" name="s" id="search" placeholder="Search…" value="<?php the_search_query(); ?>" />
+                <a class="button" href="gina-lioti-cooking-club.html">COOKING CLUB</a>
+            </div>
         </div>
-        <div class="small-3 columns">
-          <input type="submit" id="searchsubmit" value="Go" class="button postfix" />
-        </div>
-      </div>
-    </form>
+    </header>
 
-  </nav>
+    <nav class="hide-for-large">
+        <ul>
+            <li>
+                <a href="index.html" class="icon ion-home active" title="Home"></a>
+                
+            </li>
+            <li>
+                
+                <a href="search.html" class="icon ion-search" title="Search"></a>
+            </li>
+            <li>
+                
+                <a href="recipes.html" title="Recipes">Recipes</a>
+            </li>
+            <li class="show-for-medium">
+                <a href="ingredients.html">Ingredients</a>
+            </li>
+            <li>
+                <a href="gina-lioti.html" class="show-for-medium">Gina Lioti</a>
+                <a href="gina-lioti.html" class="show-for-small-only">Gina</a>
+            </li>
+        </ul>
+    </nav>
 
-  <div class="row hide-for-small-only">
-    <div class="large-12 columns">
-      <a href="/newsletter/" title="Subscribe to my newsletter">
-        <i class="ion-ios-email-outline social-icon newsletter"></i>
-      </a>
-      <a href="https://www.facebook.com/ginalioticooking" target="_blank" title="Like my page on Facebook">
-        <i class="ion-social-facebook social-icon facebook"></i>
-      </a>
-      <a href="https://twitter.com/GinaLioti" target="_blank" title="Follow me on Twitter">
-        <i class="ion-social-twitter social-icon twitter"></i>
-      </a>
-      <a href="https://www.pinterest.com/ginalioti/" target="_blank" title="Follow me on Pinterest">
-        <i class="ion-social-pinterest social-icon pinterest"></i>
-      </a>
-    </div>
-  </div>
 
-</header>
-
-<div class="row main-section">
-  <div class="large-12 columns content">
+    <main>
 
 
 
