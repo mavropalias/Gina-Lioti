0e783f38b580161537d76f7e54ac66a217b75feb
Kostas Mavropalias <mavropalias@gmail.com>
2016-11-15 14:44:52 +0000

    Finished recipes-review for homepage. Added dynamic cooking tips.

--- partial--cooking-tip.php
+++ partial--cooking-tip.php
@@ -2,6 +2,27 @@
 <section>
     <div class="row column">
         <h2>Cooking tip</h2>
-        <p class="tip">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p class="tip">
+        <?php
+            $args = array(
+                'category_name' => 'cooking-tip',
+                'posts_per_page' => 1,
+            );
+            // The query
+            $query = new WP_Query( $args );
+
+            // The Loop
+            if ( $query->have_posts() ) { ?>
+                <?php while ( $query->have_posts() ) : $query->the_post() ; ?>
+
+                    <em><a style="display: inline;" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></em> &mdash; <?php echo wp_strip_all_tags( get_the_content() ); ?>
+
+                <?php endwhile; ?>
+            
+            <?php }
+                /* Restore original Post Data */
+                wp_reset_postdata();
+        ?>
+        </p>
     </div>
 </section>
--- partial--recipes-preview.php
+++ partial--recipes-preview.php
@@ -6,25 +6,148 @@
 
     <!-- RECIPE GRID -->
     <div class="row small-up-1 medium-up-2">
-        <a href="recipe.html" class="column recipe-preview">
-            <img src="http://ginalioti.com/wp/wp-content/uploads/paprika-sauce-recipe-2304x1296.jpg">
-            <span class="recipe-title">Paprika</span>
-            <span class="recipe-subtitle">Featured</span>
-        </a>
-        <a href="recipe.html" class="column recipe-preview">
-            <img src="http://ginalioti.com/wp/wp-content/uploads/steamed-asparagus-recipe-2304x1296.jpg">
-            <span class="recipe-title">Steamed asparagus</span>
-            <span class="recipe-subtitle">Recipe of the week</span>
-        </a>
-        <a href="recipe.html" class="column recipe-preview">
-            <img src="http://ginalioti.com/wp/wp-content/uploads/basic-fluffy-quinoa-recipe-1-2304x1296.jpg">
-            <span class="recipe-title">Basic fluffy quinoa</span>
-            <span class="recipe-subtitle">New</span>
-        </a>
-        <a href="recipe.html" class="column recipe-preview">
-            <img src="http://ginalioti.com/wp/wp-content/uploads/oven-baked-lamb-with-beans-1-2304x1296.jpg">
-            <span class="recipe-title">Oven baked lamb with beans</span>
-            <span class="recipe-subtitle">New</span>
-        </a>
+    <?php
+        if(is_front_page()) {
+
+            $do_not_duplicate = array();
+            
+            // ----------------------------------------------------------------
+            // FEATURED RECIPES
+            // ----------------------------------------------------------------
+
+                $args = array(
+                    'post_type' => 'recipe',
+                    'tag' => 'featured'
+                );
+                // The query
+                $query = new WP_Query( $args );
+
+                // The Loop
+                if ( $query->have_posts() ) { ?>
+                    <?php while ( $query->have_posts() ) : $query->the_post() ; ?>
+
+                        <?php $do_not_duplicate[] = $post->ID; ?>
+
+                        <a class="column recipe-preview"
+                            href="<?php the_permalink(); ?>"
+                            title="<?php the_title_attribute(); ?>">
+                            <?php echo(get_the_post_thumbnail($post->ID, 'post-thumbnail')); ?>
+                            <span class="recipe-title"><?php the_title(); ?></span>
+                            <span class="recipe-subtitle"><?php
+                                // TAGS (Terms) 
+                                // the_terms( $post->ID, 'post_tag', '', ', ' );
+                                $terms = get_the_terms($post->ID, 'post_tag');
+                                $index = 0;
+                                foreach ( $terms as $term ) {
+                                    if ($index > 0) echo ', ';
+                                    echo $term->name;
+                                    $index++;
+                                } 
+                            ?></span>
+                        </a>
+
+                <?php endwhile; ?>
+                
+            <?php }
+                /* Restore original Post Data */
+                wp_reset_postdata();
+
+
+
+
+
+            // ----------------------------------------------------------------
+            // RECIPE OF THE WEEK
+            // ----------------------------------------------------------------
+
+                $args = array(
+                    'post_type' => 'recipe',
+                    'post__not_in' => $do_not_duplicate,
+                    'tag' => 'recipe-of-the-week'
+                );
+                // The query
+                $query = new WP_Query( $args );
+
+                // The Loop
+                if ( $query->have_posts() ) { ?>
+                    <?php while ( $query->have_posts() ) : $query->the_post() ; ?>
+
+                        <?php $do_not_duplicate[] = $post->ID; ?>
+
+                        <a class="column recipe-preview"
+                            href="<?php the_permalink(); ?>"
+                            title="<?php the_title_attribute(); ?>">
+                            <?php echo(get_the_post_thumbnail($post->ID, 'post-thumbnail')); ?>
+                            <span class="recipe-title"><?php the_title(); ?></span>
+                            <span class="recipe-subtitle"><?php
+                                // TAGS (Terms) 
+                                // the_terms( $post->ID, 'post_tag', '', ', ' );
+                                $terms = get_the_terms($post->ID, 'post_tag');
+                                $index = 0;
+                                foreach ( $terms as $term ) {
+                                    if ($index > 0) echo ', ';
+                                    echo $term->name;
+                                    $index++;
+                                } 
+                            ?></span>
+                        </a>
+
+                <?php endwhile; ?>
+                
+            <?php }
+                /* Restore original Post Data */
+                wp_reset_postdata();
+
+
+
+
+
+            // ----------------------------------------------------------------
+            // NEW RECIPES
+            // ----------------------------------------------------------------
+
+                $args = array(
+                    'post_type' => 'recipe',
+                    'post__not_in' => $do_not_duplicate,
+                    'posts_per_page' => ((count($do_not_duplicate) & 1) ? 3 : 4),
+                );
+                // The query
+                $query = new WP_Query( $args );
+
+                // The Loop
+                if ( $query->have_posts() ) { ?>
+                    <?php while ( $query->have_posts() ) : $query->the_post() ; ?>
+
+                        <?php $do_not_duplicate[] = $post->ID; ?>
+
+                        <a class="column recipe-preview"
+                            href="<?php the_permalink(); ?>"
+                            title="<?php the_title_attribute(); ?>">
+                            <?php echo(get_the_post_thumbnail($post->ID, 'post-thumbnail')); ?>
+                            <span class="recipe-title"><?php the_title(); ?></span>
+                            <span class="recipe-subtitle">New recipe<?php
+                                // TAGS (Terms) 
+                                // the_terms( $post->ID, 'post_tag', '', ', ' );
+                                $terms = get_the_terms($post->ID, 'post_tag');
+                                $index = 0;
+                                if ( $terms && ! is_wp_error( $terms ) ) {
+                                    echo ", ";
+                                    foreach ( $terms as $term ) {
+                                        if ($index > 0) echo ', ';
+                                        echo $term->name;
+                                        $index++;
+                                    } 
+                                }
+                            ?></span>
+                        </a>
+
+                <?php endwhile; ?>
+                
+            <?php }
+                /* Restore original Post Data */
+                wp_reset_postdata();
+
+        } // if(is_front_page())
+    ?>
     </div>
 </section>
