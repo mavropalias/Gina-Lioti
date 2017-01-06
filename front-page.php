61f215a48d6f4947487d1be294648f2417b2a3ef
Kostas Mavropalias <mavropalias@gmail.com>
2017-01-06 22:10:04 +0000

    misc

--- front-page.php
+++ front-page.php
@@ -2,9 +2,12 @@
 
 <!-- ABOUT GINA LIOTI -->
 <section class="section--mini">
-    <a href="hello"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/gina_lioti.jpg"></a>
+    <p>
+        <a href="hello"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/gina_lioti.jpg"></a>
+    </p>
     <div class="row columns">
-        <p class="lead">&ldquo;I mastered the Greek cuisine at its origin. I brought my experience with me to Ireland and I infused it with artisanal Irish ingredients to create deliciously healthy, gourmet, dishes.&rdquo; <a style="display:inline;" href="hello"><i class="ion-chevron-right"></i></a><p>
+        <p class="lead">&ldquo;I want to show you the absolute best of Greek cuisine. My recipes are delicious, simple and easy. &rdquo;<p>
+        <a class="button secondary button--view-more" href="/hello">Hello</a>
     </div>
 </section>
 
