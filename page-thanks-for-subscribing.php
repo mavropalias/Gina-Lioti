a542fad5ba626a1be4da992d0d341844a2615213
Kostas Mavropalias <mavropalias@gmail.com>
2017-12-30 23:02:14 +0000

    Update the subscription confirmation page

---
+++ page-thanks-for-subscribing.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<!-- COOKING CLUB PREVIEW -->
+<section class="row">
+    <div class="columns">
+        <div class="cooking-club-preview">
+            <section class="row">
+                <div class="small-12 medium-12 large-6 columns cooking-club-image"></div>
+                <div class="columns flex-container align-center align-middle">
+                    <div class="cooking-club-copy">
+                        <h3>Gina Lioti Cooking Club</h3>
+                        <p class="">
+                            &ldquo;I want to show you the absolute best of the Greek cuisine. Learn to cook delicious, healthy and easy dishes. Exclusive to members.&rdquo;
+                            <cite>Gina Lioti</cite>
+                        </p>
+                        <p class="lead">
+                            <strong>Thank you!</strong>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
